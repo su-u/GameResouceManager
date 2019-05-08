0aef3016ca865654c280bd10a0a89bb6a4731f85
すー <s1732087AP@s.chibakoudai.jp>
2019-05-08 18:48:19 +0900

    Update ProcessObserver.cs

--- GameResouceObserverFW/ProcessObserver.cs
+++ GameResouceObserverFW/ProcessObserver.cs
@@ -56,72 +56,7 @@ namespace GameResouceObserver
             //    System.Threading.Thread.Sleep(1000);
             //}
             Write("", gameProcessList);
-
-            //コンピュータ名
-            //"."はローカルコンピュータを表す
-            //コンピュータ名は省略可能（省略時は"."）
-            string machineName = ".";
-            //カテゴリ名
-            string categoryName = "Processor";
-            //カウンタ名
-            string counterName = "% Processor Time";
-            //インスタンス名
-            string instanceName = "_Total";
-
-            //カテゴリが存在するか確かめる
-            if (!System.Diagnostics.PerformanceCounterCategory.Exists(
-                categoryName, machineName))
-            {
-                Console.WriteLine("登録されていないカテゴリです。");
-                return;
-            }
-
-            //カウンタが存在するか確かめる
-            if (!System.Diagnostics.PerformanceCounterCategory.CounterExists(
-                counterName, categoryName, machineName))
-            {
-                Console.WriteLine("登録されていないカウンタです。");
-                return;
-            }
-
-            //PerformanceCounterオブジェクトの作成
-            System.Diagnostics.PerformanceCounter pc =
-                new System.Diagnostics.PerformanceCounter(
-                categoryName, counterName, instanceName, machineName);
-
-            //1秒おきに値を取得する
-            for (int i = 0; i < 10; i++)
-            {
-                //計算された値を取得し、表示する
-                Console.WriteLine(pc.NextValue());
-                //1秒待機する
-                System.Threading.Thread.Sleep(1000);
-            }
-
         }
 
-        static void Write(String text, List<System.Diagnostics.Process> ps)
-        {
-            try
-            {
-                // appendをtrueにすると，既存のファイルに追記
-                //         falseにすると，ファイルを新規作成する
-                var append = true;
-                // 出力用のファイルを開く
-                var utf8_encoding = new System.Text.UTF8Encoding(false);
-                using (var sw = new System.IO.StreamWriter(@"test.csv", append, System.Text.Encoding.UTF8))
-                {
-                    foreach (System.Diagnostics.Process p in ps)
-                    {
-                        sw.Write($"{p.ProcessName},{p.Id},{p.WorkingSet64}\n");
-                    }
-                }
-            }
-            catch (System.Exception e)
-            {
-                // ファイルを開くのに失敗したときエラーメッセージを表示
-                System.Console.WriteLine(e.Message);
-            }
-        }
     }
 }
