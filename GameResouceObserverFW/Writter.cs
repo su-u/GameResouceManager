812debecdc212b4be3da7907150e69b37382ce17
すー <s1732087AP@s.chibakoudai.jp>
2019-05-08 18:55:40 +0900

    Update Writter.cs

--- GameResouceObserverFW/Writter.cs
+++ GameResouceObserverFW/Writter.cs
@@ -8,26 +8,21 @@ namespace GameResouceManagerFW
 {
     class Writter
     {
-        static void Write(String text, List<System.Diagnostics.Process> ps)
+        static void Write(String filename, String text)
         {
             try
             {
                 // appendをtrueにすると，既存のファイルに追記
                 // falseにすると，ファイルを新規作成する
                 var append = true;
-                // 出力用のファイルを開く
                 var utf8_encoding = new System.Text.UTF8Encoding(false);
-                using (var sw = new System.IO.StreamWriter(@"test.csv", append, System.Text.Encoding.UTF8))
+                using (var sw = new System.IO.StreamWriter(filename, append, System.Text.Encoding.UTF8))
                 {
-                    foreach (System.Diagnostics.Process p in ps)
-                    {
-                        sw.Write($"{p.ProcessName},{p.Id},{p.WorkingSet64}\n");
-                    }
+                    sw.Write(text);
                 }
             }
             catch (System.Exception e)
             {
-                // ファイルを開くのに失敗したときエラーメッセージを表示
                 System.Console.WriteLine(e.Message);
             }
         }
