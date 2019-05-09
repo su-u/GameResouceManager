d47b3e7efeb7b530369735d179fc31fb08d141bf
すー <aaaiiiuuu35@gmail.com>
2019-05-09 13:35:52 +0900

    rename

---
+++ GameResouceObserverFW/Observer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace GameResouceManagerFW
+{
+    class Observer
+    {
+        
+
+        static void Main(String[] args)
+        {
+            SystemObserver so = new SystemObserver();
+            so.Measurement();
+        }
+
+        public void Communicate()
+        {
+            _communicateLoop = true;
+
+            var _ = Task.Run(() =>
+            {
+                while (_communicateLoop)
+                {
+                    try
+                    {
+                        // クライアントとの送信・受信処理
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogWarning("[Server] " + e);
+                    }
+                }
+            });
+        }
+
+    }
+}
