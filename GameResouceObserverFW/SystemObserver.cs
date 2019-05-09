b1cdb6e9bced3dd72ea435d159d2e9cf582e7034
すー <aaaiiiuuu35@gmail.com>
2019-05-09 14:06:08 +0900

    Update SystemObserver.cs

--- GameResouceObserverFW/SystemObserver.cs
+++ GameResouceObserverFW/SystemObserver.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Management;
 
-namespace GameResouceManagerFW
+namespace GameResouceObserver
 {
     class SystemObserver
     {
