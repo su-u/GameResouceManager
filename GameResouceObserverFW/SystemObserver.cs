3ece398041709c52230b869f9403d5ca00397906
すー <aaaiiiuuu35@gmail.com>
2019-05-09 13:16:00 +0900

    total memor

--- GameResouceObserverFW/SystemObserver.cs
+++ GameResouceObserverFW/SystemObserver.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Management;
 
 namespace GameResouceManagerFW
 {
@@ -18,35 +20,23 @@ namespace GameResouceManagerFW
         System.Diagnostics.PerformanceCounter pcCpu;
         System.Diagnostics.PerformanceCounter pcMem;
 
-        public Double Procesor { private set; get; }
+        public Double usingProcesor { private set; get; }
+        public Double usingMemory { private set; get; }
+
+        public readonly Double totalVisibleMemorySize;
+
 
         public SystemObserver()
         {
-            this.pcCpu = new System.Diagnostics.PerformanceCounter(catCpu, countCpu, instanceName);
-            this.pcMem = new System.Diagnostics.PerformanceCounter(catMem, countMem);
+            this.pcCpu = new PerformanceCounter(catCpu, countCpu, instanceName);
+            this.pcMem = new PerformanceCounter(catMem, countMem);
 
-            ManagementClass mc = new System.Management.ManagementClass("Win32_OperatingSystem");
+            ManagementClass mc = new ManagementClass("Win32_OperatingSystem");
             System.Management.ManagementObjectCollection moc = mc.GetInstances();
 
             foreach (System.Management.ManagementObject mo in moc)
             {
-                //合計物理メモリ
-                Console.WriteLine("合計物理メモリ:{0:#.##}MB", Convert.ToDouble(mo["TotalVisibleMemorySize"]) / 1024.0);
-                //利用可能な物理メモリ
-                Console.WriteLine("利用可能物理メモリ:{0:#.##}MB", Convert.ToDouble(mo["FreePhysicalMemory"]) / 1024.0);
-                //合計仮想メモリ
-                Console.WriteLine("合計仮想メモリ:{0:#.##}MB", Convert.ToDouble(mo["TotalVirtualMemorySize"]) / 1024.0);
-                //利用可能な仮想メモリ
-                Console.WriteLine("利用可能仮想メモリ:{0:#.##}MB", Convert.ToDouble(mo["FreeVirtualMemory"]) / 1024.0);
-
-                //他のページをスワップアウトせずにページングファイルにマップできるサイズ
-                Console.WriteLine("FreeSpaceInPagingFiles:{0}KB", mo["FreeSpaceInPagingFiles"]);
-                //ページングファイルに保存できる合計サイズ
-                Console.WriteLine("SizeStoredInPagingFiles:{0}KB", mo["SizeStoredInPagingFiles"]);
-                //スワップスペースの合計サイズ
-                //スワップスペースとページングファイルが区別されていなければ、NULL
-                Console.WriteLine("TotalSwapSpaceSize:{0}KB", mo["TotalSwapSpaceSize"]);
-
+                this.totalVisibleMemorySize = Convert.ToDouble(mo["TotalVisibleMemorySize"]);
                 mo.Dispose();
             }
 
@@ -60,8 +50,9 @@ namespace GameResouceManagerFW
             for (int i = 0; i < 10; i++)
             {
                 //計算された値を取得し、表示する
-                Console.WriteLine($"{this.pcCpu.NextValue()}%");
-                Console.WriteLine($"{this.pcMem.NextValue()}MB");
+                this.Update();
+                Console.WriteLine($"{this.usingProcesor}%");
+                Console.WriteLine($"{this.usingMemory}MB");
                 //1秒待機する
                 System.Threading.Thread.Sleep(1000);
             }
@@ -69,7 +60,29 @@ namespace GameResouceManagerFW
 
         private void Update()
         {
-            this.pcCpu.NextValue();
+            this.usingProcesor = this.pcCpu.NextValue();
+            this.usingMemory = this.pcMem.NextValue();
+        }
+
+        private void PrintManagementObject(System.Management.ManagementObject mo)
+        {
+            //合計物理メモリ
+            Console.WriteLine("合計物理メモリ:{0:#.##}MB", Convert.ToDouble(mo["TotalVisibleMemorySize"]) / 1024.0);
+            //利用可能な物理メモリ
+            Console.WriteLine("利用可能物理メモリ:{0:#.##}MB", Convert.ToDouble(mo["FreePhysicalMemory"]) / 1024.0);
+            //合計仮想メモリ
+            Console.WriteLine("合計仮想メモリ:{0:#.##}MB", Convert.ToDouble(mo["TotalVirtualMemorySize"]) / 1024.0);
+            //利用可能な仮想メモリ
+            Console.WriteLine("利用可能仮想メモリ:{0:#.##}MB", Convert.ToDouble(mo["FreeVirtualMemory"]) / 1024.0);
+
+            //他のページをスワップアウトせずにページングファイルにマップできるサイズ
+            Console.WriteLine("FreeSpaceInPagingFiles:{0}KB", mo["FreeSpaceInPagingFiles"]);
+            //ページングファイルに保存できる合計サイズ
+            Console.WriteLine("SizeStoredInPagingFiles:{0}KB", mo["SizeStoredInPagingFiles"]);
+            //スワップスペースの合計サイズ
+            //スワップスペースとページングファイルが区別されていなければ、NULL
+            Console.WriteLine("TotalSwapSpaceSize:{0}KB", mo["TotalSwapSpaceSize"]);
+            mo.Dispose();
         }
     }   
 }
